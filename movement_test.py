f15bb84030334793e9f8d3e0179b3c575cb583f6
Ethan <ethan.hawk@valpo.edu>
2021-02-02 13:15:32 -0600

    minor changes; made screen bigger

--- movement_test.py
+++ movement_test.py
@@ -3,7 +3,7 @@ import sys
 from pygame.locals import *
 
 # RGB values
-WHITE = (255, 255, 255)
+BG = (255, 255, 255)
 BLACK = (0, 0, 0)
 
 # how many pixels the character or bullet can move per frame
@@ -11,8 +11,10 @@ MOVEMENT_SPEED = 5
 BULLET_SPEED = 10
 
 # frames per second
-FPS = 30
+FPS = 60
 
+HEIGHT = 600
+WIDTH = 800
 
 def main():
     global FPSCLOCK, DISPLAYSURF
@@ -21,24 +23,25 @@ def main():
     # Create clock object
     FPSCLOCK = pygame.time.Clock()
     # set up window
-    DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+    DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
     pygame.display.set_caption("A and D to move. Space to Shoot")
 
     # load image and sound files from filepath strings
-    cat_img = pygame.image.load('images/cat.png')
-    laser_sound = pygame.mixer.Sound('sounds/laser-gun-19sf.mp3')
+    cat_img = pygame.image.load('cat.png')
+    laser_sound = pygame.mixer.Sound('laser-gun-19sf.mp3')
 
     # set initial position of cat
     cat_x = 10
-    cat_y = 220
+    cat_y = (.8 * HEIGHT)
 
     is_moving_left = False
     is_moving_right = False
+    is_moving_up = False
     bullets = []
 
     # main game loop
     while True:
-        DISPLAYSURF.fill(WHITE)
+        DISPLAYSURF.fill(BG)
 
         # display cat image at position
         DISPLAYSURF.blit(cat_img, (cat_x, cat_y))
@@ -53,8 +56,13 @@ def main():
                 cat_x = 10
         elif is_moving_right:
             cat_x += MOVEMENT_SPEED
-            if cat_x >= 280:
-                cat_x = 280
+            if cat_x >= WIDTH:
+                cat_x = WIDTH
+        elif is_moving_up:
+            cat_y -= MOVEMENT_SPEED
+            if cat_y <= 10:
+                cat_y = 10
+
 
         # respond to user input events
         for event in pygame.event.get():
@@ -63,7 +71,7 @@ def main():
                 sys.exit()
             elif event.type == KEYUP and event.key == K_SPACE: # user presses spacebar
                 create_bullet(cat_x + 40, cat_y - 15, bullets)
-                laser_sound.play()
+                # laser_sound.play()
             elif event.type == KEYDOWN and event.key == K_a: # presses A
                 is_moving_left = True
             elif event.type == KEYUP and event.key == K_a: # releases A
@@ -72,7 +80,7 @@ def main():
                 is_moving_right = True
             elif event.type == KEYUP and event.key == K_d: # releases d
                 is_moving_right = False
-
+            # add in ability to move up and down
         pygame.display.update()
 
         # increment clock. Call at very end of game loop, once per iteration
