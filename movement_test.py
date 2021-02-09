9afc4c1702c5010f51d8b47b83b915aa4249c45b
Alex Luke <59987740+calexluke@users.noreply.github.com>
2021-02-09 12:10:17 -0600

    refactored to use classes from acm_game module
    commented out code having to do with angle and rotation until we get it working

--- movement_test.py
+++ movement_test.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from pygame.locals import *
+from acm_game import *
 
 # RGB values
 BG = (255, 255, 255)
@@ -18,6 +19,7 @@ WIDTH = 800
 
 DISPLAYSURF = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 
+
 # Current TODO:
 # - have the player angle impact the distance travelled
 # - use the current mouse position to set the player angle
@@ -26,15 +28,6 @@ def game():
     global FPSCLOCK
     pygame.init()
 
-    player_angle = 0
-    # set initial position of player
-    player_x = (.4 * WIDTH)
-    player_y = (.66 * HEIGHT)
-    is_moving_left = False
-    is_moving_right = False
-    is_moving_forward = False
-    is_moving_backward = False
-
     # Create clock object
     FPSCLOCK = pygame.time.Clock()
     # set up window
@@ -42,10 +35,14 @@ def game():
     pygame.display.set_caption("WASD to move. Space to Shoot")
 
     # load image and sound files from filepath strings
-    player_img = pygame.image.load('player.png')
-    # laser_sound = pygame.mixer.Sound('laser-gun-19sf.mp3')
+    player_img = pygame.image.load('images/player.png')
+    # laser_sound = pygame.mixer.Sound('sounds/laser-gun-19sf.mp3')
+
+    # create player object with initial location. Size is approximate based on image file
+    player = Player(pygame.Rect(.4 * WIDTH, .66 * HEIGHT, 125, 80), DISPLAYSURF, player_img)
 
     bullets = []
+    enemies = []
 
     # game game loop
     while True:
@@ -53,58 +50,43 @@ def game():
         # set background color
         DISPLAYSURF.fill(BG)
         # create a player surface, and rotate the player image the appropriate number of degrees
-        PLAYER_SURF = pygame.transform.rotate(player_img, player_angle)
+        # player_angle = 0
+        # PLAYER_SURF = pygame.transform.rotate(player_img, player_angle)
         # display player image at position
-        DISPLAYSURF.blit(PLAYER_SURF, (player_x, player_y))
+        # DISPLAYSURF.blit(PLAYER_SURF, (player_x, player_y))
 
-        if len(bullets) != 0:
-            animate_bullets(bullets)
-
-        # adjust player's position
-        # TODO: factor in player_angle to movement
-        if is_moving_left:
-            player_x -= MOVEMENT_SPEED
-            if player_x <= 10:
-                player_x = 10
-        elif is_moving_right:
-            player_x += MOVEMENT_SPEED
-            if player_x >= WIDTH:
-                player_x = WIDTH
-        elif is_moving_forward:
-            player_y -= MOVEMENT_SPEED
-            if player_y <= 10:
-                player_y = 10
-        elif is_moving_backward:
-            player_y += MOVEMENT_SPEED
-            if player_y >= HEIGHT:
-                player_y = HEIGHT
-
+        player.animate()
+        for bullet in bullets:
+            bullet.animate()
+            if bullet.rect.centery <= 0:
+                # remove bullet when it reaches the top of the screen
+                del bullet
 
         # respond to user input events
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
-            elif event.type == KEYUP and event.key == K_SPACE: # user presses spacebar
-                create_bullet(player_x + 40, player_y - 15, bullets)
+            elif event.type == KEYUP and event.key == K_SPACE:  # user releases spacebar
+                player.shoot(bullets )
                 # laser_sound.play()
-            elif event.type == KEYDOWN and event.key == K_a: # presses A
-                is_moving_left = True
-            elif event.type == KEYUP and event.key == K_a: # releases A
-                is_moving_left = False
-            elif event.type == KEYDOWN and event.key == K_d: # presses D
-                is_moving_right = True
-            elif event.type == KEYUP and event.key == K_d: # releases d
-                is_moving_right = False
+            elif event.type == KEYDOWN and event.key == K_a:  # presses A
+                player.is_moving_left = True
+            elif event.type == KEYUP and event.key == K_a:  # releases A
+                player.is_moving_left = False
+            elif event.type == KEYDOWN and event.key == K_d:  # presses D
+                player.is_moving_right = True
+            elif event.type == KEYUP and event.key == K_d:  # releases d
+                player.is_moving_right = False
                 # below is to move player forward and backward, same logic as above
             elif event.type == KEYUP and event.key == K_w:
-                is_moving_forward = False
+                player.is_moving_up = False
             elif event.type == KEYDOWN and event.key == K_w:
-                is_moving_forward = True
+                player.is_moving_up = True
             elif event.type == KEYUP and event.key == K_s:
-                is_moving_backward = False
+                player.is_moving_down = False
             elif event.type == KEYDOWN and event.key == K_s:
-                is_moving_backward = True
+                player.is_moving_down = True
 
         pygame.display.update()
 
@@ -112,22 +94,8 @@ def game():
         FPSCLOCK.tick(FPS)
 
 
-def create_bullet(x_position, y_position, bullets_list):
-    # add bullet rectangle to the bullets list
-    bullets_list.append(pygame.Rect(x_position, y_position, 10, 10))
-
-
-def animate_bullets(bullets_list):
-    # draw bullets and advance their position.
-    for bullet in bullets_list:
-        pygame.draw.rect(DISPLAYSURF, BLACK, bullet)
-        bullet.centery -= BULLET_SPEED
-        if bullet.centery <= 0:
-            # remove bullet when it reaches the top of the screen
-            del bullet
-
 def welcome():
-    red = (255,0,0)
+    red = (255, 0, 0)
     load_game = False
     while (not load_game):
         DISPLAYSURF.fill(red)
@@ -135,6 +103,7 @@ def welcome():
             if event.type == MOUSEBUTTONDOWN:
                 load_game = True
         pygame.display.flip()
+
 
 def main():
     welcome()
