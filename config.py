d55649ebb4d0aeb0c99d282579d8a8d28bd96a4f
Ethan <ethan.hawk@valpo.edu>
2021-02-21 14:22:09 -0600

    Created config.py; this is so we have a central location for configuation changes, espescially if we want to try out making 'difficulties' later on

---
+++ config.py
@@ -0,0 +1,15 @@
+# RGB values
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+BG = WHITE
+
+# frames per second
+FPS = 60
+
+WINDOW_HEIGHT = 600
+WINDOW_WIDTH = 800
+
+player_img = pygame.image.load('assets/player.png')
+enemy_img = pygame.image.load('assets/enemy.png')
+
+scoreboardFont = pygame.freetype.SysFont('Comic Sans MS', 50, bold=True)
