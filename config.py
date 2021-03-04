387e443c8e7692e8adbcc7242caf3fb8c805fc3e
Nathan Harmon <22229517+nathanharmon1@users.noreply.github.com>
2021-03-04 09:32:53 -0600

    use .wav file instead of .mp3 for sound, pygame can only support .ogg and .wav, organize assets into one area

--- config.py
+++ config.py
@@ -9,7 +9,10 @@ FPS = 60
 WINDOW_HEIGHT = 600
 WINDOW_WIDTH = 800
 
-player_img = pygame.image.load('assets/player.png')
-enemy_img = pygame.image.load('assets/enemy.png')
+# TODO: These currently don't do anything. The actual configuration is on line 29 of movement_test.py
+player_img = pygame.image.load('assets/enemy.png')
+enemy_img = pygame.image.load('assets/player.png')
+background_img = pygame.image.load('assets/background.png')
+title_img = pygame.image.load('assets/title.png')
 
 scoreboardFont = pygame.freetype.SysFont('Comic Sans MS', 50, bold=True)
